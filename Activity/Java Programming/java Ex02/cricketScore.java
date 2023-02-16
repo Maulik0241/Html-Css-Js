669c8b8d8b4ad10afd6297b63f15ff6b88ebe350
Maulik <Maulik.Parmar@einfochips.com>
2023-02-17 00:32:01 +0530

    javaEx02 updated

--- Activity/Java Programming/java Ex02/cricketScore.java
+++ Activity/Java Programming/java Ex02/cricketScore.java
@@ -2,32 +2,60 @@ import java.util.*;
 
 public class cricketScore {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number of element");
-    int n = sc.nextInt();
-    if(n<0){
-        System.out.println("Invalid array size");
-        return;
-    }
-    int a[] = new int[n];
-    for (int i=0;i<n;i++){
-        System.out.println("Enter "+(i+1)+"Element");
-        a[i] = sc.nextInt();
-        if(a[i]<0){
-            System.out.println("Invalid Input");
+        Scanner sc = new Scanner(System.in);
+
+       
+        int n = sc.nextInt();
+
+       
+        if (n < 0) {
+            System.out.println("Invalid array size");
             return;
         }
-    }
-    int score = sc.nextInt();
-    if(score<0){
-        System.out.println("Invalid Score");
-        return;
-    }
-    for(int i=1;i<a.length;i+=2){
-        if(a[i]>score){
-            System.out.println(a[i-1]);
+
+        
+        int[] scores = new int[n];
+
+        // Read in the array elements
+        for (int i = 0; i < n; i++) {
+            int score = sc.nextInt();
+            
+            if (score < 0) {
+                System.out.println("Invalid input");
+                return;
+            }
+            scores[i] = score;
+        }
+
+        
+        int givenScore = sc.nextInt();
+
+        
+        if (givenScore < 0) {
+            System.out.println("Invalid score");
+            return;
+        }
+
+        
+        int[] highScorers = new int[n];
+        int count = 0;
+
+  
+        for (int i = 0; i < n; i++) {
+            if (scores[i] > givenScore) {
+                highScorers[count] = i; 
+                count++;
+            }
+        }
+
+
+        int[] output = new int[count];
+        for (int i = 0; i < count; i++) {
+            output[i] = highScorers[i];
+        }
+
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i] + " ");
         }
     }
-    
-}
 }
